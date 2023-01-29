53bae21e944aff544290dc255de4de8fbffa0e50
amir-h777 <amirhosaenfiroozi@gmail.com>
2023-01-29 23:39:09 +0330

    pref: commit 12 for develop

--- src/main/java/com/routaa/hospital/bl/doctor/controller/DoctorController.java
+++ src/main/java/com/routaa/hospital/bl/doctor/controller/DoctorController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-//adding eleveth changes to feature-hospital branch
+//adding twelveth changes to feature-hospital branch
 
 @RestController
 public class DoctorController {
