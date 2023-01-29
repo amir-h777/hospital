fb6965d4a8387c404778da29a68d92c6d2e694ec
amir-h777 <firooziamirhosaen1381@gmail.com>
2023-01-29 17:30:03 +0330

    perf: performing first commit 5 on feature-hospital

--- src/main/java/com/routaa/hospital/bl/doctor/controller/DoctorController.java
+++ src/main/java/com/routaa/hospital/bl/doctor/controller/DoctorController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-//adding fourth changes to develop branch
+//adding fifth changes to feature-hospital branch
 
 @RestController
 public class DoctorController {
