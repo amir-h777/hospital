f72f8680055c58ae326f252073db25da6a9435fc
Amirhosein Firoozi <77096382+amir-h777@users.noreply.github.com>
2023-01-30 08:52:40 +0330

    Update DoctorController.java

--- src/main/java/com/routaa/hospital/bl/doctor/controller/DoctorController.java
+++ src/main/java/com/routaa/hospital/bl/doctor/controller/DoctorController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-//adding sixteenth change to develop branch
+//adding sevebteenth change to develop branch
 
 @RestController
 public class DoctorController {
